6182bc9f55d54bfcf2b82b3bde5e2d61e713c446
KianBadie <kianbadie@gmail.com>
2020-07-30 20:23:20 -0700

    Add method to search repositories.

--- github-util.js
+++ github-util.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const _ = require('lodash');
 
 class GitHubUtil {
 
@@ -10,19 +11,25 @@ class GitHubUtil {
         }
     }
 
-    async getIssueCommentsSync(issueCommentsUrl, parameters){
+    /**
+     * Method to make request with pagination with given request url and parameters
+     * @param  {String} requestUrl      [GitHub API endpoint to request]
+     * @param  {Object} parameters      [Dictionary of parameters to be included in API request]
+     * @return {Array}                  [Array of data objects relevant to requestUrl]
+     */
+    async fetchAllSync(requestUrl, parameters={}) {
         // Construct request url with given parameters
-        let requestUrl = parameters ? `${issueCommentsUrl}?`: issueCommentsUrl;
+        requestUrl = parameters ? `${requestUrl}?`: requestUrl;
         for(let parameter in parameters){
             requestUrl = requestUrl.concat(`${parameter}=${parameters[parameter]}&`);
         }
-        let issueCommentsRes = await axios.get(requestUrl, this.config);
+        let res = await axios.get(requestUrl, this.config);
         // Return results immediataly if there is only 1 page of results.
-        if(!issueCommentsRes.headers.link){
-            return Promise.resolve(issueCommentsRes.data);
+        if(!res.headers.link){
+            return Promise.resolve(res.data);
         }
         // Get page relation links from header of response and make request for next page of comments
-        let linkRelations = issueCommentsRes.headers.link.split(',').map(function(item) {
+        let linkRelations = res.headers.link.split(',').map(function(item) {
             return item.trim();
         });
         for(let linkRel of linkRelations){
@@ -32,10 +39,34 @@ class GitHubUtil {
             link = link.substring(1, link.length - 1);
             if(rel == 'rel="next"'){
                 // Make recursive call to same method to get next page of comments
-                return issueCommentsRes.data.concat(await this.getIssueCommentsSync(link));
+                return res.data.concat(await this.getIssueCommentsSync(link));
             }
         }
-        return issueCommentsRes.data;
+        return res.data;
+    }
+
+    /**
+     * Method to make search repositories with given keywords, qualifiers, and parameters
+     * @param  {Array} searchKeywords   [Keywords to include in request]
+     * @param  {Array} qualifiers       [Array of qualifiers objects of type {qualifier: value}]
+     * @param  {Object} parameters      [Dictionary of parameters to be included in API request]
+     * @return {Object}                 [Object of {total_count, incomplete_results, items} where items is the data being requested]
+     */
+    async searchRepositories({ searchKeywords=[], qualifiers=[], parameters={} } = {}){
+        let endpoint = 'https://api.github.com/search/repositories';
+        // Construct query parameter for request url
+        let q = '';
+        for(let keyword of searchKeywords){
+            q = q.concat(`${keyword}+`);
+        }
+        for(let qualifier of qualifiers){
+            let [ qualifierName ] = Object.keys(qualifier);
+            q = q.concat(`${qualifierName}:${qualifier[qualifierName]}+`);
+        }
+        // Add query parameter to parameters object after creating a deep copy of it
+        let parametersCopy = _.clone(parameters);
+        parametersCopy.q = q;
+        return await this.fetchAllSync(endpoint, parametersCopy);
     }
 }
 
--- util-test.js
+++ util-test.js
@@ -2,7 +2,8 @@ const environment = require('dotenv').config();
 const GitHubUtil = require('./github-util');
 
 let tester = new GitHubUtil(process.env.token);
-tester.getIssueCommentsSync('https://api.github.com/repos/hackforla/website/issues/comments', {per_page:100,since:'2020-07-27T11:01:05.000Z'})
-    .then(function(data){
-        console.log(data.length);
-    });
+tester.searchRepositories({
+    qualifiers: [{topic: 'hack-for-la'}]
+}).then(function(data){
+    console.log(data.items.length);
+});
